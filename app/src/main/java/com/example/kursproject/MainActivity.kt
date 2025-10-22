bec9c663e228d77926c5ae694abefce0eed70362
rakdav <rakdav@inbox.ru>
2025-10-22 13:30:35 +0200

    TaskListScreen-2

--- app/src/main/java/com/example/kursproject/MainActivity.kt
+++ app/src/main/java/com/example/kursproject/MainActivity.kt
@@ -9,14 +9,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.kursproject.screens.TaskEditScreen
 import com.example.kursproject.screens.TaskListScreen
 import com.example.kursproject.ui.theme.KursProjectTheme
+import com.example.kursproject.viewmodels.TaskViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +45,19 @@ class MainActivity : ComponentActivity() {
                                 navController.navigate("taskEdit/${task.id}")
                             }
                         )
+                    }
+                    composable("taskEdit") {
+                        TaskEditScreen (onBack = { navController.popBackStack() })
+                    }
+                    composable("taskEdit/{taskId}") { backStackEntry ->
+                        val taskId = backStackEntry.arguments?.getString("taskId") ?: ""
+                        val viewModel: TaskViewModel= viewModel()
+                        val task by viewModel.task.collectAsState()
+                        LaunchedEffect (taskId) {
+                            viewModel.getTask(taskId)
+                        }
+                        if(task!=null)
+                            TaskEditScreen(task,onBack = { navController.popBackStack() })
                     }
                 }
             }
